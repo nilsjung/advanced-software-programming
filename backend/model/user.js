e4055f45c04ad3ef78e8b1f467d07179b3f82c40
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-09-30 16:50:15 +0200

    Task: added user schema file

---
+++ backend/model/user.js
@@ -0,0 +1,23 @@
+/**
+ * The user model.
+ * A user has:
+ *  @param {String} firstname
+ *  @param {String} lastname
+ *  @param {String} email
+ *  @param {Number} age
+ */
+
+let mongoose = require('mongoose');
+/**
+ *
+ */
+var userSchema =  new mongoose.Schema({
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    age: Number,
+});
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
