ecc075d2aeb2a8c5c573227867f324dc047d4518
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-10-23 23:07:09 +0200

    Task: add comments

--- app/src/api.js
+++ app/src/api.js
@@ -3,6 +3,11 @@ import io from 'socket.io-client';
 
 let socket = null;
 
+/**
+ * Adds the chat middleware to send messages via ws to the server.
+ *
+ * @param {Object} store the current store
+ */
 export function chatMiddleware(store) {
     return (next) => (action) => {
         if (socket && (action.type === actions.ADD_MESSAGE)) {
@@ -12,6 +17,13 @@ export function chatMiddleware(store) {
     }
 }
 
+/**
+ * Creates a connection to the server via ws.
+ *
+ * TODO save host as env or global var
+ *
+ * @param {Object} store the current store
+ */
 export default function(store) {
     socket = io.connect('http://localhost:5001');
 
