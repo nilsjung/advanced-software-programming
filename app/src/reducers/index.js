da24f2fa8bf920904a1a46a8c50b51f13464f1ad
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-11-24 16:38:22 +0100

    Task: refactor reducer swtich

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -37,47 +37,49 @@ import initialState from '../store/';
  * This is the main reducer. delegates the work to the specialized sub-reducer.
  */
 export default function(state = initialState, action) {
-    // registration reducer
-    if (
-        action.type === REGISTRATION_FAILED ||
-        action.type === REGISTRATION_SUCCESS
-    ) {
-        return registrationIsSuccess(state, action);
-    } else if (action.type === IS_LOADING) {
-        return registrationIsLoading(state, action);
+    switch (action.type) {
+        case REGISTRATION_FAILED || REGISTRATION_SUCCESS:
+            return registrationIsSuccess(state, action);
 
-        // user login
-    } else if (action.type === LOADING) {
-        return loginIsLoadingReducer(state, action);
-    } else if (action.type === SUCCESS || action.type === FAILED) {
-        return isLoginSuccessfullReducer(state, action);
-    } else if (action.type === LOGOUT) {
-        return initialState;
-        // message reducer
-    } else if (action.type === ADD_MESSAGE || action.type === ADD_RESPONSE) {
-        return messagesReducer(state, action);
-    } else if (action.type === UPDATE_MESSAGE) {
-        return updateMessageReducer(state, action);
+        case IS_LOADING:
+            return registrationIsLoading(state, action);
 
-        // user reducer
-    } else if (action.type === SET_USER_ID) {
-        return setUserIdReducer(state, action);
+        case LOADING:
+            return loginIsLoadingReducer(state, action);
 
-        // logging reducer
-    } else if (action.type === SHOW_INFO_MESSAGE) {
-        return setInfoMessage(state, action);
-    } else if (action.type === RESET_INFO_MESSAGE) {
-        return resetInfoMessage(state, action);
+        case SUCCESS || FAILED:
+            return isLoginSuccessfullReducer(state, action);
+
+        case LOGOUT:
+            return initialState;
+
+        case ADD_MESSAGE || ADD_RESPONSE:
+            return messagesReducer(state, action);
+
+        case UPDATE_MESSAGE:
+            return updateMessageReducer(state, action);
+
+        case SET_USER_ID:
+            return setUserIdReducer(state, action);
+
+        case SHOW_INFO_MESSAGE:
+            return setInfoMessage(state, action);
+
+        case RESET_INFO_MESSAGE:
+            return resetInfoMessage(state, action);
+
+        case CHANGE_ROOM:
+            return { ...state, currentChatroom: action.currentChatroom };
+
+        case CREATE_CHATROOM:
+            const newChatrooms = Object.create(state.chatrooms);
+            newChatrooms.push(action.chatroom);
+            return { ...state, chatrooms: newChatrooms };
+
+        case LOAD_HISTORY:
+            return { ...state, messages: action.chats };
+
+        default:
+            return { ...state };
     }
-    //chatroom reducer
-    else if (action.type === CHANGE_ROOM) {
-        return { ...state, currentChatroom: action.currentChatroom };
-    } else if (action.type === CREATE_CHATROOM) {
-        const newChatrooms = Object.create(state.chatrooms);
-        newChatrooms.push(action.chatroom);
-        return { ...state, chatrooms: newChatrooms };
-    } else if (action.type === LOAD_HISTORY) {
-        return { ...state, messages: action.chats };
-    }
-    return { ...state };
 }
