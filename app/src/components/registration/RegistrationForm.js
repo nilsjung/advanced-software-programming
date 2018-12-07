fb1542c8a16951480fa5b0599e37815acfa933d5
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-12-07 15:21:26 +0100

    Task: move registration

---
+++ app/src/components/registration/RegistrationForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import Input from '../mixins/Input';
+import Button from '../mixins/Button';
+
+class RegistrationForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: {
+                firstname: '',
+                lastname: '',
+                password: '',
+                email: '',
+            },
+        };
+    }
+
+    onChange = (key) => (value) => {
+        const newValue = value;
+        this.setState(() => {
+            return {
+                user: {
+                    [key]: newValue,
+                },
+            };
+        });
+    };
+
+    handleSubmit = (event) => {
+        this.props.onSubmit({ user: this.state.user });
+        event.preventDefault();
+    };
+
+    render() {
+        return (
+            <form>
+                <Input
+                    onChange={this.onChange('email')}
+                    label="Email Address"
+                    Id="registrationEmail"
+                    placeholder="Email"
+                />
+                <Input
+                    onChange={this.onChange('firstname')}
+                    label="First Name"
+                    Id="firstName"
+                    placeholder="Enter your first name"
+                />
+                <Input
+                    onChange={this.onChange('lastname')}
+                    label="Last Name"
+                    Id="lastName"
+                    placeholder="Enter your last name"
+                />
+                <Input
+                    onChange={this.onChange('password')}
+                    type="password"
+                    label="Password"
+                    Id="password"
+                    placeholder="Enter your password"
+                />
+                <Button onClick={this.handleSubmit} label="Registration" />
+            </form>
+        );
+    }
+}
+
+export default RegistrationForm;
