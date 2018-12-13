c1da4c2fdcb7f878ceaf7d99ddbd351a41b7f5a2
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-12-13 17:29:46 +0100

    Task: structure of tests

--- backend/test/user.js
+++ backend/test/user.js
@@ -2,7 +2,13 @@ const userTests = (app, chai, url) => {
     let User = require('../model/user');
     const userEndpoint = url + '/user/';
 
-    describe('User', () => {
+    const removeUser = (done) => {
+        User.deleteMany({}, () => {
+            done();
+        });
+    };
+
+    describe('user endpoint', () => {
         let MaxMusterman = {
             firstname: 'Max',
             email: 'max.mustermann@testmail.com',
@@ -10,190 +16,203 @@ const userTests = (app, chai, url) => {
             password: '12345678',
         };
 
-        beforeEach((done) => {
-            User.remove({}, (err) => {
-                done();
-            });
-        });
+        describe('with a non logged in user', () => {
+            before(removeUser);
 
-        before((done) => {
-            chai.request(app)
-                .post(userEndpoint)
-                .send(MaxMusterman)
-                .end((err, res) => {
-                    const { firstname, lastname } = res.body.user;
-                    console.log(`${firstname} ${lastname} registered..`);
-                });
-            done();
-        });
-
-        after((done) => {
-            chai.request(app)
-                .delete(userEndpoint)
-                .end((err, res) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log(res.body.message);
-                    }
-                });
-            done();
-        });
-
-        /**
-         * Tests for GET all
-         */
-        describe('/GET user', () => {
-            it('it should GET all users', (done) => {
-                chai.request(app)
-                    .get(userEndpoint)
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.body.should.be.a('array');
-                        res.body.length.should.be.eql(0);
-                        done();
-                    });
-            });
-        });
-
-        /**
-         * Tests for GET /users/:id
-         */
-        describe('/GET/:id', () => {
-            it('it should GET a user by the given ID', (done) => {
-                let user = new User(MaxMusterman);
-                user.save((err, user) => {
-                    chai.request(app)
-                        .get(userEndpoint + user._id)
-                        .end((err, res) => {
-                            res.should.have.status(200);
-                            res.body.should.be.a('object');
-                            res.body.should.have.property('firstname');
-                            res.body.should.have.property('lastname');
-                            res.body.should.have.property('email');
-                            res.body.should.have.property('password');
-                            res.body.should.have.property('_id').eql(user.id);
-                            done();
-                        });
-                });
-            });
-        });
-
-        /**
-         * Tests for POST
-         */
-        describe('/Post user', () => {
-            it('it creates a new user', (done) => {
-                chai.request(app)
-                    .post(userEndpoint)
-                    .send(MaxMusterman)
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.body.should.be.a('object');
-                        res.body.should.have
-                            .property('message')
-                            .eql('user created');
-                        res.body.user.should.have.property('lastname');
-                        res.body.user.should.have.property('firstname');
-                        res.body.user.should.have.property('email');
-                        done();
-                    });
-            });
-
-            it('it should not create two users with the same email', (done) => {
-                const user = new User(MaxMusterman);
-                user.save(() => {
+            /**
+             * Tests for POST
+             */
+            describe('Post /user', () => {
+                it('it should create a new user', (done) => {
                     chai.request(app)
                         .post(userEndpoint)
-                        .send(user)
+                        .send(MaxMusterman)
                         .end((err, res) => {
                             res.should.have.status(200);
                             res.body.should.be.a('object');
                             res.body.should.have
                                 .property('message')
-                                .eql('email already in use');
+                                .eql('user created');
+                            res.body.user.should.have.property('lastname');
+                            res.body.user.should.have.property('firstname');
+                            res.body.user.should.have.property('email');
+                            done();
+                        });
+                });
+
+                it('it should not create two users with the same email', (done) => {
+                    const user = new User(MaxMusterman);
+                    user.save(() => {
+                        chai.request(app)
+                            .post(userEndpoint)
+                            .send(user)
+                            .end((err, res) => {
+                                res.should.have.status(200);
+                                res.body.should.be.a('object');
+                                res.body.should.have
+                                    .property('message')
+                                    .eql('email already in use');
+                                done();
+                            });
+                    });
+                });
+
+                it('it should create no user if arguments are missing', (done) => {
+                    let max = { ...MaxMusterman };
+                    delete max.lastname;
+
+                    chai.request(app)
+                        .post(userEndpoint)
+                        .send(max)
+                        .end((err, res) => {
+                            res.should.have.status(400);
+                            res.body.should.be.a('object');
+                            res.body.should.have
+                                .property('message')
+                                .eql('data not complete');
                             done();
                         });
                 });
             });
 
-            it('it creates no user if arguments are missing', (done) => {
-                let max = { ...MaxMusterman };
-                delete max.lastname;
+            describe('POST /user/login', () => {
+                beforeEach((done) => {
+                    let max = new User(MaxMusterman);
+                    max.save(() => {});
+                    done();
+                });
 
-                chai.request(app)
-                    .post(userEndpoint)
-                    .send(max)
-                    .end((err, res) => {
-                        res.should.have.status(400);
-                        res.body.should.be.a('object');
-                        res.body.should.have
-                            .property('message')
-                            .eql('data not complete');
-                        done();
-                    });
+                it('it should log in an existing user', (done) => {
+                    chai.request(app)
+                        .post(userEndpoint + 'login')
+                        .send(MaxMusterman)
+                        .end((err, res) => {
+                            res.should.have.status(200);
+                            res.body.should.be.a('object');
+                            res.body.should.have
+                                .property('message')
+                                .eql('successfully logged in');
+                            res.body.should.have.property('user');
+                            res.body.user.should.have.property('email');
+                            res.body.user.should.have.property('password');
+                            done();
+                        });
+                });
+
+                it('it should not log in an user with a wrong password', (done) => {
+                    let max = { ...MaxMusterman, password: 'wrongPassword' };
+                    chai.request(app)
+                        .post(userEndpoint + 'login')
+                        .send(max)
+                        .end((err, res) => {
+                            res.should.have.status(403);
+                            res.body.should.have.property('user').eql(null);
+                            res.body.should.have
+                                .property('message')
+                                .eql('invalid password');
+                            done();
+                        });
+                });
+
+                it('it should not log in an non existing user', (done) => {
+                    const MaxMusterfrau = {
+                        firstname: 'Maxime',
+                        lastname: 'Musterfrau',
+                        password: '222333',
+                        email: 'maxime@musterfrau.com',
+                    };
+
+                    chai.request(app)
+                        .post(userEndpoint + 'login')
+                        .send(MaxMusterfrau)
+                        .end((err, res) => {
+                            res.should.have.status(404);
+                            res.body.should.be.a('object');
+                            res.body.should.have
+                                .property('error')
+                                .eql('user not found');
+                            res.body.should.not.have.property('user');
+                            done();
+                        });
+                });
             });
         });
 
-        describe('/login user', () => {
-            beforeEach((done) => {
-                let max = new User(MaxMusterman);
-                max.save(() => {});
-                done();
-            });
+        describe('with a logged in user', () => {
+            // remove existing users
+            beforeEach(removeUser);
 
-            it('it should log in an existing user', (done) => {
+            var token = '';
+            const authHeader = 'X-Custom-Authorisation';
+            const secureGetRequest = (path) =>
+                chai
+                    .request(app)
+                    .get(path)
+                    .set(authHeader, token);
+
+            before((done) => {
+                chai.request(app)
+                    .post(userEndpoint)
+                    .send(MaxMusterman)
+                    .end((err, res) => {});
+
+                // login a user to get the token
                 chai.request(app)
                     .post(userEndpoint + 'login')
                     .send(MaxMusterman)
                     .end((err, res) => {
+                        token = res.body.token;
+                    });
+                done();
+            });
+
+            /**
+             * Tests for GET all
+             */
+            describe('GET /user', () => {
+                it('it should GET all users', (done) => {
+                    secureGetRequest(userEndpoint).end((err, res) => {
                         res.should.have.status(200);
-                        res.body.should.be.a('object');
-                        res.body.should.have
-                            .property('message')
-                            .eql('successfully logged in');
-                        res.body.should.have.property('user');
-                        res.body.user.should.have.property('email');
-                        res.body.user.should.have.property('password');
+                        res.body.should.be.a('array');
+                        res.body.length.should.be.eql(0);
                         done();
                     });
+                });
             });
 
-            it('it should not log in an user with a wrong password', (done) => {
-                let max = { ...MaxMusterman, password: 'wrongPassword' };
-                chai.request(app)
-                    .post(userEndpoint + 'login')
-                    .send(max)
-                    .end((err, res) => {
-                        res.should.have.status(403);
-                        res.body.should.have.property('user').eql(null);
-                        res.body.should.have
-                            .property('message')
-                            .eql('invalid password');
-                        done();
+            /**
+             * Tests for GET /users/:id
+             */
+            describe('GET /user/:id', () => {
+                it('it should GET a user by the given ID', (done) => {
+                    let user = new User(MaxMusterman);
+                    user.save((err, user) => {
+                        secureGetRequest(userEndpoint + user._id).end(
+                            (err, res) => {
+                                res.should.have.status(200);
+                                res.body.should.be.a('object');
+                                res.body.should.have.property('firstname');
+                                res.body.should.have.property('lastname');
+                                res.body.should.have.property('email');
+                                res.body.should.have.property('password');
+                                res.body.should.have
+                                    .property('_id')
+                                    .eql(user.id);
+                                done();
+                            }
+                        );
                     });
+                });
             });
 
-            it('it should not log in an non existing user', (done) => {
-                const MaxMusterfrau = {
-                    firstname: 'Maxime',
-                    lastname: 'Musterfrau',
-                    password: '222333',
-                    email: 'maxime@musterfrau.com',
-                };
+            describe('DELETE /user/:id', () => {
+                it('it should delete the user with given ID', (done) => {
+                    done();
+                });
 
-                chai.request(app)
-                    .post(userEndpoint + 'login')
-                    .send(MaxMusterfrau)
-                    .end((err, res) => {
-                        res.should.have.status(404);
-                        res.body.should.be.a('object');
-                        res.body.should.have
-                            .property('error')
-                            .eql('user not found');
-                        res.body.should.not.have.property('user');
-                        done();
-                    });
+                it('it should inform if no user is found', (done) => {
+                    done();
+                });
             });
         });
     });
