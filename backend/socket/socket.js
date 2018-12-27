baec5cb7c58fedc80b2374f0b25fedc93690efb2
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-12-27 13:29:57 +0100

    Task: refactor backend message socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,5 +1,5 @@
-const token = require('./../security/token');
-const chatroomService = require('./../services/chatroomService');
+const messageService = require('./messages');
+const onlineStatusService = require('./onlinestatus');
 
 const socket = (server) => {
     let userId = 0;
@@ -13,31 +13,14 @@ const socket = (server) => {
         connections.push(socket);
         userId += 1;
         socket.emit('start', { userId });
-        socket.on('onlinestatus', (userid, onlinestatus) => onlineStatusService(socket, userid, onlinestatus))
+        socket.on('message', (data) => messageService(data, socket));
+        socket.on('onlinestatus', (userid, onlinestatus) =>
+            onlineStatusService(socket, userid, onlinestatus)
+        );
         socket.on('disconnect', () => disconnectService(connections));
     });
 
     return io;
-};
-
-const messageService = async (data, connections) => {
-    const user = await token.verify(data.token);
-    const chatroom = data.chatroom;
-
-    //store message to chat
-    chatroomService
-        .storeMessageToChatroom(data.message, data.user, chatroom)
-        .then((result) => {})
-        .catch((err) => console.warn(err));
-
-    connections.forEach((connectedSocket) => {
-        if (connectedSocket !== socket) {
-            connectedSocket.emit('message', {
-                message: data.message,
-                user: data.user,
-            });
-        }
-    });
 };
 
 const disconnectService = (connections) => {
