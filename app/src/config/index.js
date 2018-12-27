5b2119035d92f640eff7b89c4f721932ecea16f4
Nils Jung <nils.m.jung@student.fh-kiel.de>
2018-12-27 11:56:51 +0100

    Task: add onlinestatus to state

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -4,3 +4,7 @@ const port = 5001;
 export const HOST = 'http://' + domain + ':' + port + '/api/';
 export const WS = 'http://' + domain + ':' + port;
 
+export const onlinestatus = {
+    OFFLINE: 'offline',
+    ONLINE: 'online',
+};
