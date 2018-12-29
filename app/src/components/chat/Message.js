60489576e39c4a3ab8eee086ad96a7032cbadaf5
Marc <marcenge@gmail.com>
2018-12-29 18:37:15 +0100

    YoutubeUrls eingefügt

--- app/src/components/chat/Message.js
+++ app/src/components/chat/Message.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import moment from 'moment';
+import React from 'react';
 import { DateFormat } from '../../config';
 
 const formatDate = (d) => {
@@ -16,8 +16,34 @@ const Message = (props) => {
                 <span className="MessageAuthor col-8">{props.userName}</span>
             </small>
             <div className="MessageBody">{props.text}</div>
+            {retrieveYoutubeURLs(props.text)}
         </li>
     );
 };
 
+function retrieveYoutubeURLs(text) {
+    const urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+    const urls = text.match(urlRegex);
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    let returnValue = [];
+    if (urls != null) {
+        urls.forEach((element) => {
+            const match = element.match(regExp);
+            if (match && match[2].length == 11) {
+                const youtubeUrl = 'https://www.youtube.com/embed/' + match[2];
+                returnValue.push(
+                    <div class="plyr__video-embed" id="player">
+                        <iframe
+                            src={youtubeUrl}
+                            allowfullscreen
+                            allowtransparency
+                            allow="autoplay"
+                        />
+                    </div>
+                );
+            }
+        });
+        return returnValue;
+    }
+}
 export default Message;
