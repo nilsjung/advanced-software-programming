22b3bd743b06b5680ca0f63a4985d8dcd1ad7b5e
jule.m.martensen@student.fh-kiel.de <DidG-HPv2018>
2018-12-31 11:55:38 +0100

    add DateFormat to config

--- app/src/config/index.js
+++ app/src/config/index.js
@@ -1,6 +1,7 @@
 const domain = 'localhost';
 const port = 5001;
 
+export const DateFormat = 'MM/DD/YY h:mm';
 export const HOST = 'http://' + domain + ':' + port + '/api/';
 export const WS = 'http://' + domain + ':' + port;
 
@@ -8,4 +9,3 @@ export const onlinestatus = {
     OFFLINE: 'offline',
     ONLINE: 'online',
 };
-
