71cf97b79961177057324bda2fd3d53c36d8e92a
Nils Jung <nils.m.jung@student.fh-kiel.de>
2019-01-03 12:16:33 +0100

    Refactor: save complete user in frontend for _id.

--- app/src/reducers/userReducer.js
+++ app/src/reducers/userReducer.js
@@ -6,14 +6,9 @@ export function setUserIdReducer(state, action) {
 
 export function isLoginSuccessfullReducer(state, action) {
     const { user, accessToken, chatrooms, userchats } = action;
-    const copiedState = Object.create(state);
-    const newUser = copiedState.user;
-    newUser.email = user.email;
-    newUser.firstname = user.firstname;
-    newUser.lastname = user.lastname;
     return {
         ...state,
-        user: newUser,
+        user: { ...user },
         accessToken,
         chatrooms,
         userchats,
