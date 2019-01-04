93321d04d163b24f0627a1c6dca56cce9d855f57
Marc <marcenge@gmail.com>
2019-01-04 16:37:38 +0100

    übersetzung demo

--- app/src/components/chat/Message.js
+++ app/src/components/chat/Message.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { MultiLineParser } from 'text-emoji-parser';
 import { DateFormat } from '../../config';
 import { Emoji } from 'emoji-mart';
+
+const url = 'https://translate.yandex.net/api/v1.5/tr.json/translate',
+    keyAPI =
+        'trnsl.1.1.20190103T181139Z.7464426976138302.da0198e0cc346b526c18e9ffa40aa8144de2282d';
+
 const formatDate = (d) => {
     return moment(d).format(DateFormat);
 };
